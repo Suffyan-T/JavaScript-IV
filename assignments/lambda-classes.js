b0a9e5137fc3db6365ec1e1e2f58a54cd99155c4
Suffyan Tariq <suffyan.tariq@gmail.com>
2019-07-25 16:35:53 -0700

    started work on classes file

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,12 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(user){
+        this.name = user.name;
+        this.age = user.age;
+        this.location = user.location;
+        this.gender = user.gender;
+    }
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
+    }
+}
